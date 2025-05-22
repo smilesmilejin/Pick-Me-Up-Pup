06add75af127394da74c0327243a1a7c2c93d1a6
Ashley Yang <ashley.minghui@gmail.com>
2025-05-22 12:05:19 -0700

    Add inifitiny scroll implementation, refactor code to make functions more single responsibility and organized

--- src/index.js
+++ src/index.js
@@ -14,25 +14,70 @@ const pickRandomNumber = () => {
   return Math.floor(Math.random() * NUMBER_OF_DOGS) + 1;
 };
 
-const handleAddButtonClicked = (event) => {
-  const imgNumber = pickRandomNumber();
+// Extract logic related to building a new dog img element
+// Combines making a new element and updating the count since we want the count 
+// to reflect the number of images created
+const createNewDogImageElement = (imgNumber) => {
   const newImgName = `../imgs/${imgNumber}.jpeg`;
   const newDog = document.createElement('img');
   newDog.src = newImgName;
   newDog.alt = 'A random doggo!';
-  state.dogImageContainer.prepend(newDog);
   ++state.dogCount;
+
+  return newDog;
+};
+
+const getRandomDogImage = () => {
+  const imgNumber = pickRandomNumber();
+  const newDog = createNewDogImageElement(imgNumber);
+
+  return newDog;
+};
+
+// Extract logic related to updating UI to its own helper
+const refreshUI = () => {
   state.dogCountLabel.textContent = state.dogCount;
+};
+
+const addDogImgToTop = () => {
+  const newDog = getRandomDogImage();
+  state.dogImageContainer.prepend(newDog);
+  refreshUI();
+};
+
+const addDogImgToBottom = () => {
+  {
+    const newDog = getRandomDogImage();
+    state.dogImageContainer.append(newDog);
+    refreshUI();
+  }
+};
+
+// Callback function passed to `addEventListener` for the click event is now
+// very concise when logic is refactored into descriptively named helper functions
+const handleAddButtonClicked = (event) => {
+  addDogImgToTop();
+};
+
+// Infinity Scroll Implementation (NOT shown in roundtable)
+// This is the callback function passed to `addEventListener` for the scroll event
+const handlePageScrolled = (event) => {
+  // Determine if we've reached the end of the document by
+  // checking if the amount we've scrolled from the top plus
+  // the visible part of the screen is greater than
+  // or equal to the document's scrollHeight
+  if (
+    window.scrollY + window.innerHeight + 1 >=
+    document.documentElement.scrollHeight
+  ) {
+    addDogImgToBottom();
+  }
 };
 
 const registerEvents = () => {
   state.addDogButton.addEventListener('click', handleAddButtonClicked);
-};
-
-const onLoaded = () => {
-  // steps to carry out when the page has loaded
-  loadControls();
-  registerEvents();
+  // add event listener for the scroll event for infinite scroll feature
+  window.addEventListener('scroll', handlePageScrolled);
 };
 
 const loadControls = () => {
@@ -40,6 +85,12 @@ const loadControls = () => {
   state.addDogButton = document.getElementById('addDogButton');
   state.dogImageContainer = document.getElementById('dog-img-container');
   state.dogCountLabel = document.getElementById('dogCountLabel');
+};
+
+const onLoaded = () => {
+  // steps to carry out when the page has loaded
+  loadControls();
+  registerEvents();
 };
 
 onLoaded();
